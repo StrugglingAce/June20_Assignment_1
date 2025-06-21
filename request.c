630f329dc6f218f865f6b1b5465b4ac5cbe8ad11
StrugglingAce <chibuzor.awanye@triosstudent.com>
2025-06-20 22:04:46 -0400

    even more additions

--- request.c
+++ request.c
@@ -2,6 +2,10 @@
 
 int main(void)
 {
-    printf("Hello World");
+    int x = 8;
+    int y = 1;
+    int z = x+y;
+    printf("Hello World\n");
+    printf("The value of x + y = %d\n" z);
     return 0;
 }
